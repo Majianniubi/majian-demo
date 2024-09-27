66cc3721655c586ce40e588bfefce8b1d659a83c
majian <majian@qq.com>
2024-09-27 13:26:23 +0800

    master commit

--- src/main/java/com/example/testgit/TestGitApplication.java
+++ src/main/java/com/example/testgit/TestGitApplication.java
@@ -11,5 +11,6 @@ public class TestGitApplication {
         System.out.println("hello majian");
         System.out.println("helli majian2");
         System.out.println("hot-fix test");
+        System.out.println("master test");
     }
 }
